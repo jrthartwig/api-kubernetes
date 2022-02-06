94798bce86fa2681edccce3fd33bfce651e08e42
Jocelynn Hartwig <jocelynnhartwig@gmail.com>
2022-02-06 18:50:49 -0500

    fix: api routing

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express(); 
 
-app.get('/articles', (req, res) => {
+app.get('/api/articles', (req, res) => {
     res.json([
         {
             title: 'HP', author: 'JK'
